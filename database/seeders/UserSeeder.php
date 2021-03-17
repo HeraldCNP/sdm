2ffab2a0f40b62fab18c3c7d9b3e9f36f7c015a5
Herald Choque <heraldcnp@gmail.com>
2021-03-17 15:10:20 -0400

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Herald Choque Vargas',
+            'email' => 'heraldcnp@gmail.com',
+            'password' => bcrypt('123'),
+        ]);
+        User::factory(99)->create();
+    }
+}
